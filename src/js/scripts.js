0979e7de4b3cf515ba474017115486a758f009eb
erick <ecastillo@mcllent.com>
2025-02-25 13:17:13 -0600

    actualizacion imagenes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -146,7 +146,7 @@
 
 
 		// SLIDER
-		var menu = ['WHO WE ARE<span>OUR PASSION</span>', 'NOTHERN LIGHTS<span>SPECIAL OFFER</span>', 'BEST SELLING<span>ICE CLIMBING</span>', 'THE FAMOUS<span>HIKING TRAILS</span>'];
+		var menu = ['EXPLORA LA <span>CULTURA PERUANA</span>', 'PARAÍSO<span>GRIEGO</span>', 'AVENTURA EN<span>PAISAJES ÉPICOS</span>', 'AVENTURA<span>AUSTRALIANA</span>'];
 		var interleaveOffset = 0.5;
 		var swiperOptions = {
 			loop: true,
